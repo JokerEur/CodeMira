1ddb6a3b0ebff1dd0390581c0c55cfd15dcb368f
Alina Burykina <alina4268@mail.ru>
2023-05-26 16:56:01 +0300

    database0.2

--- backend/database.py
+++ backend/database.py
@@ -1,189 +1,160 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, ForeignKey, Text
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy.ext.declarative import declared_attr
-from sqlalchemy.sql.sqltypes import DateTime
+from peewee import *
+from time import sleep
 
-engine = create_engine('sqlite:///db.sqlite3', echo=True)
-Base = declarative_base(bind=engine)
+db = SqliteDatabase('../db.sqlite3')
 
 
-class BaseModel:
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
+class BaseModel(db.Model):
+    class Meta:
+        database = db
 
 
-class Role(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    role_name = Column(String)
+class Role(BaseModel):
+    id = IntegerField(primary_key=True)
+    role_name = CharField()
 
 
-class BankRequisition(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    card_number = Column(String)
+class BankRequisition(BaseModel):
+    id = IntegerField(primary_key=True)
+    card_number = CharField()
 
 
-class SubscriptionType(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    type = Column(String)
+class SubscriptionType(BaseModel):
+    id = IntegerField(primary_key=True)
+    type = CharField()
 
 
-class Okved(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    code = Column(Integer)
-    name = Column(String)
+class Okved(BaseModel):
+    id = IntegerField(primary_key=True)
+    code = IntegerField()
+    name = CharField()
 
 
-class Users(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String)
-    password_hash = Column(String)
-    phone = Column(String)
-    role_id = Column(Integer, ForeignKey('role.id'))
-    role = relationship(Role, backref='users')
-    created_at = Column(DateTime)
-    subscription_on = Column(Boolean)
-    subscription_id = Column(Integer, nullable=True)
+class User(BaseModel):
+    id = IntegerField(primary_key=True)
+    first_name = CharField()
+    last_name = CharField()
+    email = CharField()
+    password_hash = CharField()
+    phone = CharField()
+    role = ForeignKeyField(Role, field='id', backref='users')
+    created_at = TimestampField()
+    subscription_on = BooleanField()
+    subscription_id = IntegerField(null=True)
+    # starred_products = TextField(null=True)
 
 
-class Seller(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(Users, backref='sellers')
-    card_number_id = Column(Integer, ForeignKey('bankrequisition.id'))
-    card_number = relationship(BankRequisition, backref='sellers')
-    last_order = Column(Integer, nullable=True)
-    is_ip = Column(Boolean)
-    address = Column(String)
+class Seller(BaseModel):
+    id = IntegerField(primary_key=True)
+    user_id = ForeignKeyField(User, field='id', backref='sellers')
+    card_number = ForeignKeyField(BankRequisition, field='id', backref='sellers')
+    last_order = IntegerField(null=True)
+    is_ip = BooleanField()
+    address = CharField()
 
 
-class OkvedList(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    okved_id = Column(Integer, ForeignKey('okved.id'))
-    okved = relationship(Okved, backref='okved_lists')
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(Users, backref='okved_lists')
+class OkvedList(BaseModel):
+    id = IntegerField()
+    okved = ForeignKeyField(Okved, field='id', backref='okved_lists')
+    user_id = ForeignKeyField(User, field='id', backref='okved_lists')
 
 
-class ProductCategory(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+class ProductCategory(BaseModel):
+    id = IntegerField(primary_key=True)
+    name = CharField()
 
 
-class Products(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    price = Column(Float)
-    category_id = Column(Integer, ForeignKey('productcategory.id'))
-    category = relationship(ProductCategory, backref='products')
-    description = Column(String)
-    seller_id = Column(Integer, ForeignKey('seller.id'))
-    seller = relationship(Seller, backref='products')
-    origin = Column(String)
-    produced_at = Column(DateTime)
-    expiration_date = Column(DateTime)
-    rating = Column(Float)
-    license = Column(String)
-    certificate = Column(String)
-    picture = Column(Text)
+class Products(BaseModel):
+    id = IntegerField(primary_key=True)
+    name = CharField()
+    price = FloatField()
+    category = ForeignKeyField(ProductCategory, field='id', backref='products')
+    description = CharField(null=True)
+    seller = ForeignKeyField(Seller, field='id', backref='products')
+    origin = CharField(null=True)
+    produced_at = TimestampField()
+    expiration_date = TimestampField(null=True)
+    rating = FloatField()	# Select from Reting by ProductId
+    license = CharField(null=True)
+    certificate = CharField(null=True)
+    picture = TextField(null=True)
+
+class Raiting(BaseModel):
+    id = IntegerField(primary_key=True)
+    product_id = ForeignKeyField(Products, field='id', backref='raiting')
+    rate = IntegerField()
+    comment = CharField()
+    author = ForeignKeyField(User, field='id', backref='raiting')
+
+class ItemsList(BaseModel):
+    id = IntegerField()
+    product = ForeignKeyField(Products, field='id', backref='items_lists')
+    user_id = ForeignKeyField(User, field='id', backref='items_lists')
 
 
-class Rating(Base, BaseModel):
-    product_id = Column(Integer, ForeignKey('products.id'))
-    product = relationship(Products, backref='raiting')
-    rate = Column(Integer)
-    comment = Column(String)
-    author_id = Column(Integer, ForeignKey('users.id'))
-    author = relationship(Users, backref='raiting')
+class StarredProduct(BaseModel):
+    id = IntegerField()
+    product = ForeignKeyField(Products, field='id', backref='starred_products')
+    user_id = ForeignKeyField(User, field='id', backref='starred_products')
 
 
-class ItemsList(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    product_id = Column(Integer, ForeignKey('products.id'))
-    product = relationship(Products, backref='items_lists')
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(Users, backref='items_lists')
+class Chat(BaseModel):
+    id = IntegerField(primary_key=True)
+    users_first = ForeignKeyField(User, field='id', backref='chats_first')
+    users_second = ForeignKeyField(User, field='id', backref='chats_second')
 
 
-class StarredProduct(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    product_id = Column(Integer, ForeignKey('products.id'))
-    product = relationship(Products, backref='starred_products')
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(Users, backref='starred_products')
+class Message(BaseModel):
+    id = IntegerField()
+    chat = ForeignKeyField(Chat, field='id', backref='messages')
+    sender = ForeignKeyField(User, field='id', backref='messages')
+    time = TimestampField()
 
 
-class Chat(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    user_first_id = Column(Integer, ForeignKey('users.id'))
-    user_first = relationship(Users, foreign_keys=[user_first_id], backref='chats_first')
-    user_second_id = Column(Integer, ForeignKey('users.id'))
-    user_second = relationship(Users, foreign_keys=[user_second_id], backref='chats_second')
+class DeliveryType(BaseModel):
+    id = IntegerField(primary_key=True)
+    type = CharField()
 
 
-class Message(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer, ForeignKey('chat.id'))
-    chat = relationship(Chat, backref='messages')
-    sender_id = Column(Integer, ForeignKey('users.id'))
-    sender = relationship(Users, backref='messages')
-    time = Column(DateTime)
+class Subscription(BaseModel):
+    id = IntegerField(primary_key=True)
+    user_id = ForeignKeyField(User, field='id', backref='subscriptions')
+    item = TextField()
+    count = IntegerField()
+    price = FloatField()
+    subscription_type = ForeignKeyField(SubscriptionType, field='id', backref='subscriptions')
+    delivery_type = ForeignKeyField(DeliveryType, field='id', backref='subscriptions')
+    card_number = ForeignKeyField(BankRequisition, field='id', backref='subscriptions')
 
 
-class DeliveryType(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    type = Column(String)
+class Cart(BaseModel):
+    id = IntegerField(primary_key=True)
+    user_id = ForeignKeyField(User, field='id', backref='carts')
+    total_price = FloatField()
+    seller = ForeignKeyField(Seller, field='id', backref='carts')
 
 
-class Subscription(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(Users, backref='subscriptions')
-    item = Column(Text)
-    count = Column(Integer)
-    price = Column(Float)
-    subscription_type_id = Column(Integer, ForeignKey('subscriptiontype.id'), nullable=True)
-    subscription_type = relationship(SubscriptionType, backref='subscriptions')
-    delivery_type_id = Column(Integer, ForeignKey('deliverytype.id'), nullable=True)
-    delivery_type = relationship(DeliveryType, backref='subscriptions')
-    card_number_id = Column(Integer, ForeignKey('bankrequisition.id'), nullable=True)
-    card_number = relationship(BankRequisition, backref='subscriptions')
+class OrderStatus(BaseModel):
+    id = IntegerField(primary_key=True)
+    status = CharField()
 
 
-class Cart(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(Users, backref='carts')
-    total_price = Column(Float)
-    seller_id = Column(Integer, ForeignKey('seller.id'))
-    seller = relationship(Seller, backref='carts')
+class DeliveryStatus(BaseModel):
+    id = IntegerField(primary_key=True)
+    status = CharField()
 
 
-class OrderStatus(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    status = Column(String)
+class Order(BaseModel):
+    id = IntegerField(primary_key=True)
+    user_id = ForeignKeyField(User, field='id', backref='orders')
+    cart = ForeignKeyField(Cart, field='id', backref='orders')
+    status = ForeignKeyField(OrderStatus, field='id', backref='orders', null=True)
+    delivery_type = ForeignKeyField(DeliveryType, field='id', backref='orders', null=True)
+    delivery_status = ForeignKeyField(DeliveryStatus, field='id', backref='orders', null=True)
+    closed = BooleanField()
 
-
-class DeliveryStatus(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    status = Column(String)
-
-
-class Order(Base, BaseModel):
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(Users, backref='orders', nullable=True)
-    cart_id = Column(Integer, ForeignKey('cart.id'), nullable=True)
-    cart = relationship(Cart, backref='orders', nullable=True)
-    status_id = Column(Integer, ForeignKey('orderstatus.id'), nullable=True)
-    status = relationship(OrderStatus, backref='orders', nullable=True)
-    delivery_type_id = Column(Integer, ForeignKey('deliverytype.id'), nullable=True)
-    delivery_type = relationship(DeliveryType, backref='orders', nullable=True)
-    delivery_status_id = Column(Integer, ForeignKey('deliverystatus.id'), nullable=True)
-    delivery_status = relationship(DeliveryStatus, backref='orders', nullable=True)
-    closed = Column(Boolean)
-
-
-Base.metadata.create_all()
+db.create_tables([
+    Role, BankRequisition, SubscriptionType, Okved, User, Seller, OkvedList,
+    ProductCategory, Products, ItemsList, StarredProduct, Chat, Message,
+    Subscription, Cart, OrderStatus, DeliveryType, DeliveryStatus, Order, Raiting
+])
